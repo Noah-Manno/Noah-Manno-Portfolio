89bfc51801851a9f7dfc3824bc42c1ef37f590f6
Noah Manno <noahmanno19@gmail.com>
2025-04-23 07:25:43 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   const toggleDarkMode = () => setDarkMode(prev => !prev);
 
   return (
-    <Router basename="/Noah-Manno-Portfolio">
+    <Router>
       <SideMenu darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Routes>
         <Route path="/" element={<Hero darkMode={darkMode} toggleDarkMode={toggleDarkMode} />} />
